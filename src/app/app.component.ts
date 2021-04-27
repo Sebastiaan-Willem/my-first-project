5698aa38a8e4ac5b992237041f4a19b954bcce9c
Michiel Wouters <michiel_woute64@hotmail.com>
2021-04-27 11:31:52 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'Hello Angular!!!';
+  teaPot = 'I am a little teapot.';
+  description = 'Short and stout.';
+
+  num1 = 5;
+  num2 = 7;
+  bool = false;
+  // num3 = this.num1 + this.num2
 }
