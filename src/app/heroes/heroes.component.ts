4af16c4e5f1ec69ecaa6fb0f0d955aa1a0ddc3c9
Michiel Wouters <michiel_woute64@hotmail.com>
2021-04-27 13:34:41 +0200

    Added Property & Method binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = "Windstorm";
+  hero: Hero = {
+    id: 1,
+    name: 'windstorm',
+    imagePath: ""
+  };
 
-  constructor() { }
+  clicked = 0;
+
+  HandleClicked() {
+    if (this.clicked % 2 === 0) {
+      this.hero.imagePath = "http://placekitten.com/400/600"
+    }
+    else {
+      this.hero.imagePath = "http://placekitten.com/600/400"
+    }
+
+    this.clicked++;
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
