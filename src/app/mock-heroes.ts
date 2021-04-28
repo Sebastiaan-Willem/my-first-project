6fe6072c119bd14f5b1fbe88f0743965b12cc817
Michiel Wouters <michiel_woute64@hotmail.com>
2021-04-28 09:16:34 +0200

    Mockheroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,10 +1,10 @@
 import { Hero } from './Hero';
 
 export const HEROES:Hero[] = [
-    {id: 11, name: 'Dr Nice', imagePath: "http://placekitten.com/400/600"},
+    {id: 11, name: 'Dr. Nice', imagePath: "http://placekitten.com/400/600"},
     {id: 12, name: 'Iron man', imagePath: "http://placekitten.com/400/600"},
     {id: 13, name: 'Spiderman', imagePath: "http://placekitten.com/400/600"},
     {id: 14, name: 'Batman', imagePath: "http://placekitten.com/400/600"},
-    {id: 15, name: 'Dr Not Nice', imagePath: "http://placekitten.com/400/600"},
-    {id: 16, name: 'Dr Unpleasant', imagePath: "http://placekitten.com/400/600"},
+    {id: 15, name: 'Dr. Not Nice', imagePath: "http://placekitten.com/400/600"},
+    {id: 16, name: 'Dr. Unpleasant', imagePath: "http://placekitten.com/400/600"},
 ];
