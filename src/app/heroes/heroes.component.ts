17e6bb47428e7f350d5f97edbb25856d90b7c12e
Michiel Wouters <michiel_woute64@hotmail.com>
2021-04-30 10:49:34 +0200

    Injected Hero Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,33 +8,41 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  selectedHero = this.heroes[1];
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
   isImageVisible = true;
 
   clicked = 0;
+
+  constructor(private heroService: HeroService) {
+  }
+
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   OnSelect(hero: Hero) {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
   HandleClicked() {
-    if (this.clicked % 2 === 0) {
-      this.selectedHero.imagePath = "http://placekitten.com/400/600"
-      this.isImageVisible = true;
+    if (this.selectedHero) {
+      if (this.clicked % 2 === 0) {
+        this.selectedHero.imagePath = "http://placekitten.com/400/600"
+        this.isImageVisible = true;
+      }
+      else {
+        this.selectedHero.imagePath = "http://placekitten.com/600/400"
+        this.isImageVisible = false;
+      }
+
+      this.clicked++;
     }
-    else {
-      this.selectedHero.imagePath = "http://placekitten.com/600/400"
-      this.isImageVisible = false;
-    }
-
-    this.clicked++;
   }
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
 }
