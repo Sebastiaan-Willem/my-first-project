01a0e5979b2fc112d55a3fa7d0dfe6a47b451426
Michiel Wouters <michiel_woute64@hotmail.com>
2021-04-30 14:23:21 +0200

    Updated changes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,9 +2,13 @@ import { Hero } from './hero';
 
 export const HEROES:Hero[] = [
     {id: 11, name: 'Dr. Nice', imagePath: "http://placekitten.com/400/600"},
-    {id: 12, name: 'Iron man', imagePath: "http://placekitten.com/400/600"},
-    {id: 13, name: 'Spiderman', imagePath: "http://placekitten.com/400/600"},
-    {id: 14, name: 'Batman', imagePath: "http://placekitten.com/400/600"},
-    {id: 15, name: 'Dr. Not Nice', imagePath: "http://placekitten.com/400/600"},
-    {id: 16, name: 'Dr. Unpleasant', imagePath: "http://placekitten.com/400/600"},
+    {id: 12, name: 'Narco', imagePath: "http://placekitten.com/400/600"},
+    {id: 13, name: 'Bombasto', imagePath: "http://placekitten.com/400/600"},
+    {id: 14, name: 'Celeritas', imagePath: "http://placekitten.com/400/600"},
+    {id: 15, name: 'Magneta', imagePath: "http://placekitten.com/400/600"},
+    {id: 16, name: 'RubberMan', imagePath: "http://placekitten.com/400/600"},
+    {id: 17, name: 'Dynama', imagePath: "http://placekitten.com/400/600"},
+    {id: 18, name: 'Dr. IQ', imagePath: "http://placekitten.com/400/600"},
+    {id: 19, name: 'Magma', imagePath: "http://placekitten.com/400/600"},
+    {id: 20, name: 'Tornado', imagePath: "http://placekitten.com/400/600"},
 ];
